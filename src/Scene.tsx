ad7830a520307704527fea6e3ec4d536e1f00ef7
Lorenzo Andraghetti <andraghetti.l@gmail.com>
2026-01-07 20:52:14 +0100

    New model 2.0

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1204,8 +1204,9 @@ function setupLighting(scene: THREE.Scene) {
   keyLight.shadow.camera.near = 0.1
   keyLight.shadow.camera.far = 15
   keyLight.shadow.camera.fov = 45
-  keyLight.shadow.bias = -0.0001
-  keyLight.shadow.normalBias = 0.02
+  keyLight.shadow.bias = config.shadowBias
+  keyLight.shadow.normalBias = config.shadowNormalBias
+  keyLight.shadow.radius = config.shadowRadius
   scene.add(keyLight)
   scene.add(keyLight.target)
 
@@ -1225,9 +1226,9 @@ function setupLighting(scene: THREE.Scene) {
   fillLight.shadow.camera.right = 10
   fillLight.shadow.camera.top = 10
   fillLight.shadow.camera.bottom = -10
-  fillLight.shadow.bias = -0.0001
-  fillLight.shadow.normalBias = 0.02
-  fillLight.shadow.radius = 8
+  fillLight.shadow.bias = config.shadowBias
+  fillLight.shadow.normalBias = config.shadowNormalBias
+  fillLight.shadow.radius = config.shadowRadius
   scene.add(fillLight)
   scene.add(fillLight.target)
 
@@ -1247,9 +1248,9 @@ function setupLighting(scene: THREE.Scene) {
   backLight.shadow.camera.right = 10
   backLight.shadow.camera.top = 10
   backLight.shadow.camera.bottom = -10
-  backLight.shadow.bias = -0.0001
-  backLight.shadow.normalBias = 0.02
-  backLight.shadow.radius = 8
+  backLight.shadow.bias = config.shadowBias
+  backLight.shadow.normalBias = config.shadowNormalBias
+  backLight.shadow.radius = config.shadowRadius
   scene.add(backLight)
   scene.add(backLight.target)
 
